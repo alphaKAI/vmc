e30b181d11cbebd6d82c47030a394f5236f04d7f
Akihiro Shoji <alpha.kai.net@alpha-kai-net.info>
2023-06-15 19:49:58 +0900

    improve

--- vmc_ip_reporter/src/main.rs
+++ vmc_ip_reporter/src/main.rs
@@ -1,11 +1,12 @@
-use std::{fs::File, io::prelude::*, path::Path};
+use std::{env, thread, time};
 use std::{process::Command, str};
-use std::{thread, time};
 use vmc_common::{
-    MachineInfo, NSRequest, Request, SerializedDataContainer, SERVER_HOST, SERVER_PORT, AutoReConnectTcpStream,
+    AutoReConnectTcpStream, MachineInfo, NSRequest, Request, SerializedDataContainer, ETH_NAME,
+    IP_PREFIX_LIST, SERVER_HOST, SERVER_PORT,
 };
 
 fn get_ipaddr(eth_name: &str) -> Option<String> {
+    // iproute2
     let output = Command::new("sh")
         .arg("-c")
         .arg(format!("ip addr show dev {eth_name} | grep inet"))
@@ -15,8 +16,27 @@ fn get_ipaddr(eth_name: &str) -> Option<String> {
     let output: Vec<_> = str::from_utf8(&output).unwrap().split(' ').collect();
 
     for e in &output {
-        if e.starts_with("172") || e.starts_with("192") {
-            return Some(e[0..e.len() - 3].to_string());
+        for ip_prefix in IP_PREFIX_LIST.iter() {
+            if e.starts_with(ip_prefix) {
+                return Some(e[0..e.len() - 3].to_string());
+            }
+        }
+    }
+
+    // ifconfig
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("ifconfig {eth_name} | grep inet"))
+        .output()
+        .expect("failed to execute process")
+        .stdout;
+    let output: Vec<_> = str::from_utf8(&output).unwrap().split(' ').collect();
+
+    for e in &output {
+        for ip_prefix in IP_PREFIX_LIST.iter() {
+            if e.starts_with(ip_prefix) {
+                return Some(e[0..e.len() - 3].to_string());
+            }
         }
     }
 
@@ -24,15 +44,7 @@ fn get_ipaddr(eth_name: &str) -> Option<String> {
 }
 
 fn get_hostname() -> Option<String> {
-    let path = Path::new("/etc/hostname");
-
-    if let Ok(mut file) = File::open(path) {
-        let mut s = String::new();
-        file.read_to_string(&mut s).unwrap();
-        return Some(s.trim().to_string());
-    }
-
-    None
+    env::var("HOST").ok()
 }
 
 fn main() -> std::io::Result<()> {
@@ -41,7 +53,7 @@ fn main() -> std::io::Result<()> {
     sock.set_verbosity(true);
 
     loop {
-        let (ipaddr, hostname) = (get_ipaddr("eth0").unwrap(), get_hostname().unwrap());
+        let (ipaddr, hostname) = (get_ipaddr(ETH_NAME).unwrap(), get_hostname().unwrap());
         let m = Request::NameService(NSRequest::Heartbeat(MachineInfo { hostname, ipaddr }));
         let sdc = SerializedDataContainer::from_serializable_data(&m).unwrap();
 
@@ -52,4 +64,3 @@ fn main() -> std::io::Result<()> {
         thread::sleep(sleep_sec);
     }
 }
-
